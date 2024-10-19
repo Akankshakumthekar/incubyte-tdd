2f5bd5fdfece934e2288c0e6e1cc69134549a1f8
Akankshakumthekar <akankshakumthekar@gmail.com>
2024-10-19 15:22:40 +0530

    support different delimiters

--- add.js
+++ add.js
@@ -2,7 +2,13 @@ function add(input){
     if (input ===""){
         return 0;
     } 
-    const numbers = input.replace(/\n/g,',').split(",");
+    let delimiter = /,|\n/;
+    if(input.startsWith("//")){
+        const parts = input.split("\n");
+        delimiter = new RegExp(parts[0].slice(2));
+        input = parts[1];
+    }
+    const numbers = input.split(delimiter);
     return numbers.reduce((sum, no) => sum + parseInt(no), 0);
 }
 module.exports = add;
--- add.test.js
+++ add.test.js
@@ -11,3 +11,6 @@ test('should return the sum of two comma separated numbers',() =>{
 test('should handle new lines between numbers',() =>{
     expect(add("1\n2,3")).toBe(6);
 });
+test('should support different delimiters',() =>{
+    expect(add("//;\n1;2")).toBe(3);
+})
