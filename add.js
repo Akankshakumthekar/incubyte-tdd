9c54b9a21425cb4821f2a63005b75d298a8fcda9
Akankshakumthekar <akankshakumthekar@gmail.com>
2024-10-19 15:14:19 +0530

    Handle new lines between numbers

--- add.js
+++ add.js
@@ -2,7 +2,7 @@ function add(input){
     if (input ===""){
         return 0;
     } 
-    const numbers = input.split(",");
+    const numbers = input.replace(/\n/g,',').split(",");
     return numbers.reduce((sum, no) => sum + parseInt(no), 0);
 }
 module.exports = add;
